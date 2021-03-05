87addf7abdb978f568810f8a65e6dfdc134a7892
MrAjMann <anthonyjmann87@gmail.com>
2021-03-05 14:24:47 +1000

    removed role check until It is working going to create to dev branch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
   # before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
-	before_action :check_role
+	# before_action :check_role
   def page
     
   end
 
   private
 
-  def check_role
-		if	current_user.has_role? :merchant
-				return
-		else
-				flash[:alert] = "You are not authorised"
-				redirect_to root_path
-		end
-	end
+  # def check_role
+	# 	if	current_user.has_role? :merchant
+	# 			return
+	# 	else
+	# 			flash[:alert] = "Welcome "#{customername}""
+	# 			
+	# 	end
+	# end
 end
