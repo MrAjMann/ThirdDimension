fdb04d772b9beab4b78354ab67f85346b3ca6210
MrAjMann <anthonyjmann87@gmail.com>
2021-03-18 13:56:59 +1000

    Created a user Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create :first_name => 'John',:last_name => 'Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
-User.create :first_name => 'Sally',:last_name => 'Harper', :email => 'sally@gmail.com', :password => 'hotsauce', :password_confirmation => 'hotsauce'
+
+JohnDoe = User.create(:first_name => 'John',:last_name => 'Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret')
+
+SallyHarper = (User.create :first_name => 'Sally',:last_name => 'Harper', :email => 'sally@gmail.com', :password => 'hotsauce', :password_confirmation => 'hotsauce')
+puts "Users created: #{User.all.count}"
+
+#Catagories
+
+
+
 
 
 
